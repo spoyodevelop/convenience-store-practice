49c7693e73596fa6a803d71a4aaccd2ad9513bdb
spoyodevelop <kjy9505@gmail.com>
2024-12-08 04:02:15 +0900

    feat: validator 완성

--- src/Validator/validateShoppingCart.js
+++ src/Validator/validateShoppingCart.js
@@ -1,9 +1,13 @@
 function checkAvailableQuantity(products, name, quantity) {
   let availableQuantity = 0;
   const foundProduct = products.filter((product) => product.name === name);
-  console.log(foundProduct);
+
   foundProduct.forEach((product) => (availableQuantity += +product.quantity));
   return availableQuantity > quantity;
+}
+function checkProductAvaliable(products, name) {
+  const foundProduct = products.filter((product) => product.name === name);
+  return foundProduct.length > 0;
 }
 export default function validateShoppingCart(input, products) {
   const shoppingCart = [];
@@ -27,6 +31,8 @@ export default function validateShoppingCart(input, products) {
       if (Number.isNaN(Number(quantity)) || Number(quantity) < 0) {
         throw new Error('[ERROR] 포멧이 올바르지 않습니다. ');
       }
+      if (!checkProductAvaliable(products, productName))
+        throw new Error('[ERROR] 물건이 존재하지 않습니다.');
       if (!checkAvailableQuantity(products, productName, quantity))
         throw new Error('[ERROR] 수량이 부족합니다.');
 
@@ -40,6 +46,8 @@ export default function validateShoppingCart(input, products) {
     if (Number.isNaN(Number(quantity)) || Number(quantity) < 0) {
       throw new Error('[ERROR] 포멧이 올바르지 않습니다.');
     }
+    if (!checkProductAvaliable(products, productName))
+      throw new Error('[ERROR] 물건이 존재하지 않습니다.');
     if (!checkAvailableQuantity(products, productName, quantity))
       throw new Error('[ERROR] 수량이 부족합니다.');
 
